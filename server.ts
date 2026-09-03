c55f18b4b4f34c7673ec57f226fa93e1e8b05b69
Alind <31258577+1alind@users.noreply.github.com>
2026-09-03 18:53:31 +0300

    fix: correct typo in server response message

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import http from "http";
 
 const server = http.createServer((_req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("PHP Application Workspace - All source code located in /website");
+  res.end("PHP Applicationss Workspace - All source code located in /website");
 });
 
 server.listen(3000, "0.0.0.0");
